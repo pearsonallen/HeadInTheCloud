10093e2cbb81d4c9ac1256285b0ceb78c454f8d5
chrispearson <cpearson@apterainc.com>
2020-11-20 10:23:37 -0500

    head name change

--- headincloud/src/App.js
+++ headincloud/src/App.js
@@ -11,9 +11,9 @@ function App() {
       <h1>Head In The Clouds</h1>
       <h2>Inspired By #art channel's weekly prompt:</h2>
       <h2>Sky</h2>
-      <button onClick={() => setValue(1)}>Blue 1</button>
-      <button onClick={() => setValue(2)}>Red 2</button>
-      <button onClick={() => setValue(3)}>Green 3</button>
+      <button onClick={() => setValue(1)}>Blue Head</button>
+      <button onClick={() => setValue(2)}>Red Head</button>
+      <button onClick={() => setValue(3)}>Green Head</button>
     </div>
   );
 }

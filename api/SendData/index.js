10585fdadacdc9f70848f8ff235eb75cab4656c6
chrispearson <cpearson@apterainc.com>
2020-11-20 15:37:03 -0500

    num change

--- api/SendData/index.js
+++ api/SendData/index.js
@@ -2,11 +2,11 @@ const axios = require("axios");
 module.exports = async function (context, req) {
      let value = req.query.value;
     if (value == 1) {
-        v = "14000";
+        v = "21000";
     } else if (value == 2) {
-        v = "48000";
+        v = "55000";
     } else if (value == 3) {
-        v = "80000";
+        v = "87000";
     } else {
         v = 0;
     }

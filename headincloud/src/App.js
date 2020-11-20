1a09bff7d36bc98997d60732a5b082c46be5403c
chrispearson <cpearson@apterainc.com>
2020-11-20 10:04:03 -0500

    header message

--- headincloud/src/App.js
+++ headincloud/src/App.js
@@ -9,6 +9,8 @@ function App() {
   return (
     <div className="App">
       <h1>Head In The Clouds</h1>
+      <h2>Inspired By #art channel's weekly prompt:</h2>
+      <h2>Sky</h2>
       <button onClick={() => setValue(1)}>Head 1</button>
       <button onClick={() => setValue(2)}>Head 2</button>
       <button onClick={() => setValue(3)}>Head 3</button>
